ba40f69f741d2f979bb4799b33b2ebd1f7d8b778
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-08-07 22:36:28 +0900

    feat: sample Userデータを用意

--- api/user.py
+++ api/user.py
@@ -7,7 +7,26 @@ request = {
     }
 }
 
+request_user = [
+    {
+        "data": {
+            "user": {
+                "id": 1,
+                "name": "なまえ",
+                "email": "sample@sample.com",
+                "post_number": "111-1111",
+                "address1": "大阪府",
+                "address2": "大阪市",
+                "job": "witter"
+            }
+        }
+    }
+]
+
 if __name__ == '__main__':
 
-    for k, v in request["data"].items():
+    # for k, v in request["data"].items():
+    #     print(k, v)
+
+    for k, v in request_user[0]['data']['user'].items():
         print(k, v)
