a4946ba8b356009599e0dfb848f034d4ed3f4176
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-08-08 17:04:24 +0900

    feat: テストを追加

--- test/test_user.py
+++ test/test_user.py
@@ -12,12 +12,12 @@ class TestUser(unittest.TestCase):
         self.request_data = user.request
         self.response_data = self.tf.read_response_json_file(open_file=open_file)
         # nest2
-        self.request_user = user.request_user
-        self.response_user_data = self.tf.read_response_json_file(open_file=open_file2)
+        self.request_data_nest2 = user.request_user
+        self.response_data_nest2 = self.tf.read_response_json_file(open_file=open_file2)
 
     def tearDown(self) -> None:
         del self.response_data
-        del self.response_user_data
+        del self.response_data_nest2
 
     def test_response_success1(self):
         """ 一つずつ、比較項目を書く処理 手間。。。 """
@@ -34,6 +34,22 @@ class TestUser(unittest.TestCase):
 
     def test_response_success3(self):
         """ テストファイル, ２階層ネスト """
-        self.tf.tmp_tow_nest_success_response_data(request_data=self.request_user,
-                                                   response_data=self.response_user_data,
+        self.tf.tmp_tow_nest_success_response_data(request_data=self.request_data_nest2,
+                                                   response_data=self.response_data_nest2,
                                                    idx=0, first_key='data', second_key='user')
+
+    def test_response_success_nest1(self):
+        self.tf.tmp_response_test_(nest=1,
+                                   request_data=self.request_data,
+                                   response_data=self.response_data,
+                                   first_key='data',
+                                   )
+
+    def test_response_success_nest2(self):
+        self.tf.tmp_response_test_(nest=2,
+                                   request_data=self.request_data_nest2,
+                                   response_data=self.response_data_nest2,
+                                   first_key='data',
+                                   second_key='user',
+                                   idx=0,
+                                   )
