4744a2be0ce6bcc7fcfaef3a2f6eac3f7be94d4e
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-08-14 17:02:27 +0900

    refactor: add drop, rename func.

--- api/models/db.py
+++ api/models/db.py
@@ -1,7 +1,9 @@
+import logging
 import sqlite3
 
 
 class DB(object):
+    """　DB 操作を """
 
     def __init__(self, db_name: str, table_name: str):
         self.db_name = db_name
@@ -18,41 +20,61 @@ class DB(object):
 
         try:
             cur.execute(f'''CREATE TABLE {self.table_name}
-                       (id PRIMARY KEY, date text, trans text, symbol text, qty real, price real)''')
+                       (id PRIMARY KEY,
+                        date text,
+                        trans text,
+                        symbol text,
+                        qty real,
+                        price real)'''
+                        )
+
         except sqlite3.OperationalError as ex:
             print(ex)
 
-    def insert_user(self, con, insert_num=460):
+    def query_insert(self, con, insert_num=460):
         cur = con.cursor()
 
         # Insert a row of data
         for i in range(1, insert_num + 1):
             cur.execute(f"INSERT INTO {self.table_name} VALUES ('{i}','2006-01-05','BUY_{i}','RHAT',100,35.14)")
 
-        # Save (commit) the changes
         con.commit()
 
-    def query(self, con):
+    def query_select_order_by(self, con, oder_by_name='id'):
         cur = con.cursor()
-        for row in cur.execute(f"SELECT * FROM {self.table_name} ORDER BY price"):
+        for row in cur.execute(f"SELECT * FROM {self.table_name} ORDER BY {oder_by_name}"):
             print(row)
 
     def query_write_file(self, con, file_name='test.text'):
         cur = con.cursor()
         with open(file_name, 'w', encoding='utf-8') as f:
-            for row in cur.execute(f'SELECT * FROM {self.table_name} ORDER BY price'):
+            for row in cur.execute(f"SELECT * FROM {self.table_name} ORDER BY price"):
                 write_str = f"INSERT INTO {self.table_name} VALUES {row};\n"
                 f.write(write_str)
 
+    def query_drop_(self, con):
+        cur = con.cursor()
+        cur.execute(f"DROP TABLE {self.table_name}")
+
 
 if __name__ == '__main__':
-    DB_NAME = 'test4'
+    DB_NAME = 'test'
 
-    db1 = DB(db_name=f"../../db/{DB_NAME}.db", table_name='stocks')
+    try:
+        print('start ...')
 
-    con = db1.connect_db()
-    db1.create_table(con)
-    db1.insert_user(con, insert_num=460000)
+        db1 = DB(db_name=f"../../db/{DB_NAME}.db", table_name='stocks')
 
-    db1.query_write_file(con=con, file_name=f"../../data/{DB_NAME}.sql")
-    db1.close_connect(con)
+        con = db1.connect_db()
+        db1.create_table(con)
+        db1.query_insert(con, insert_num=460000)
+
+        db1.query_write_file(con=con, file_name=f"../../data/{DB_NAME}.sql")
+        db1.query_drop_(con)
+
+        db1.close_connect(con)
+
+    except Exception as ex:
+        logging.debug(ex)
+    finally:
+        print('done')
